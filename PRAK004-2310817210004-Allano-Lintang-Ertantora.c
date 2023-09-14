1a9e9305ba600cddc9a9d3c8a445c1149f36f384
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-09-14 14:22:07 +0800

    Add files via upload

---
+++ PRAK004-2310817210004-Allano-Lintang-Ertantora.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf("##############################\n");
+    printf("#\t\t\t\t\t\t\t #\n");
+    printf("#\tAllano Lintang Ertantora #\n");
+    printf("#\t\t2310817210004\t\t #\n");
+    printf("#\t\t\t\t\t\t\t #\n");
+     printf("##############################\n");
+    return 0;
+}
