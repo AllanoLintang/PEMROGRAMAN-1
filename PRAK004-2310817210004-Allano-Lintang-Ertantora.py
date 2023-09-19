aa1469207f95965803b2e9567a3c2dec6b8d5370
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-09-19 18:05:29 +0800

    Update PRAK004-2310817210004-Allano-Lintang-Ertantora.py

--- PRAK004-2310817210004-Allano-Lintang-Ertantora.py
+++ PRAK004-2310817210004-Allano-Lintang-Ertantora.py
@@ -1,6 +1,8 @@
-print("##############################")
-print("#                            #")
-print("#  Allano Lintang Ertantora  #")
-print("#       2310817210004        #")
-print("#                            #")
-print("##############################")
+nama = "Allano Lintang Ertantora"
+NIM = "2310817210004"
+print("#"*32)
+print("#"," "*28,"#")
+print("#", nama.center(28),"#")
+print("#", NIM.center(28), "#")
+print("#"," "*28,"#")
+print("#"*32)
