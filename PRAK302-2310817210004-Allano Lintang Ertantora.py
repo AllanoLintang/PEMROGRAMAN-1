8986cb2235524580e10d6ec8e3b49c56a4544f75
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-10-30 15:07:45 +0800

    Update PRAK302-2310817210004-Allano Lintang Ertantora.py

--- PRAK302-2310817210004-Allano Lintang Ertantora.py
+++ PRAK302-2310817210004-Allano Lintang Ertantora.py
@@ -2,11 +2,11 @@ for i in range (0, 5):
     nilai = int(input())
     if nilai >= 80:
         print("A")
-    elif nilai >= 70 and nilai <= 79:
+    elif nilai >= 70:
         print("B")
-    elif nilai >= 60 and nilai <= 69:
+    elif nilai >= 60:
         print("C")
-    elif nilai >= 50 and nilai <= 59:
+    elif nilai >= 50:
         print("D")
     elif nilai <= 50:
         print("E")
