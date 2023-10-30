af1909d47431197f071b71555dc83293864191ad
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-10-30 15:04:30 +0800

    Update PRAK302-2310817210004-Allano-Lintang_Ertantora.c

--- PRAK302-2310817210004-Allano-Lintang_Ertantora.c
+++ PRAK302-2310817210004-Allano-Lintang_Ertantora.c
@@ -3,12 +3,12 @@ int main(){
     int nilai,i;
     for(i=0;i<5;i++){
     scanf("%d", &nilai);
-    if(nilai >= 80){printf("A");}
-    else if(nilai >= 70){printf("B");}
-    else if(nilai >= 60){printf("C");}
-    else if(nilai >= 50){printf("D");}
-    else if(nilai <= 50){printf("F");}
-    else{printf("nilai tidak valid!!!11!1!!!");}
+    if(nilai >= 80){printf("A\n");}
+    else if(nilai >= 70){printf("B\n");}
+    else if(nilai >= 60){printf("C\n");}
+    else if(nilai >= 50){printf("D\n");}
+    else if(nilai <= 50){printf("F\n");}
+    else{printf("nilai tidak valid!!!11!1!!!\n");}
     }
     return 0;
 }
