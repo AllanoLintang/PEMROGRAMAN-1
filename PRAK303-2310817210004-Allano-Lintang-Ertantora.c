32eff13c730cadea9d1192ee66f125abd7a88d74
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-10-31 17:00:37 +0800

    Update PRAK303-2310817210004-Allano-Lintang-Ertantora.c

--- PRAK303-2310817210004-Allano-Lintang-Ertantora.c
+++ PRAK303-2310817210004-Allano-Lintang-Ertantora.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    int N, i;
-    for(i = 0;i < 3;i++){
+    int N;
+    for(int i = 0;i < 3;i++){
     scanf("%d", &N);
     if(N >= 1){printf("Positif\n");}
     else if(N <= -1){printf("Negatif\n");}
