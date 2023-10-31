d0004054169593de433e1407fa12735d01f8c46b
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-10-31 16:51:21 +0800

    Update PRAK302-2310817210004-Allano-Lintang_Ertantora.c

--- PRAK302-2310817210004-Allano-Lintang_Ertantora.c
+++ PRAK302-2310817210004-Allano-Lintang_Ertantora.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    int nilai,i;
-    for(i=0;i<5;i++){
+    int nilai;
+    for(int i=0;i<5;i++){
     scanf("%d", &nilai);
     if(nilai >= 80){printf("A\n");}
     else if(nilai >= 70){printf("B\n");}
