6c3f60387cae91b2503647d6e0fc62e2f0ae15ec
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-10-31 16:45:34 +0800

    Update PRAK301-2310817210004-Allano-Lintang-Ertantora.c

--- PRAK301-2310817210004-Allano-Lintang-Ertantora.c
+++ PRAK301-2310817210004-Allano-Lintang-Ertantora.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-    int a, b, i;
-    for(i = 0;i < 3;i++){
+    int a, b;
+    for(int i = 0;i < 3;i++){
     scanf("%d %d", &a, &b);
     if(b<a){printf("%d %d\n", b, a);}
     else{printf("%d %d\n", a, b);}
