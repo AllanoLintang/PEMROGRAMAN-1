fb706820a4c54159d542607cc6e543c962bb5f49
Allano Lintang Ertantora <144782417+AllanoLintang@users.noreply.github.com>
2023-11-11 21:26:56 +0800

    Update PRAK404-2310817210004-Allano-Lintang-Ertantora.c

--- PRAK404-2310817210004-Allano-Lintang-Ertantora.c
+++ PRAK404-2310817210004-Allano-Lintang-Ertantora.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
+
+void kalk(float *x, float *y){
+    printf("Masukkan nilai pertama: ");
+    scanf("%f", x);
+    printf("Masukkan nilai kedua: ");
+    scanf("%f", y);
+}
 int main(){
-    float x, y, pil;
+    float x, y;
+    int pil;
     while(1){
         printf("Pilih program\n1. Penjumlahan\n2. Pengurangan\n3. Perkalian\n4. Pembagian\n5. Exit\n");
         printf("Masukkan Pilihan: ");
-        scanf("%f", &pil);
-        if(pil == 5){
-            printf("Terimakasih, telah menggunakan kalkulator Allano");
-            break;
-        }else if (pil == 1){
-            printf("Masukkan nilai pertama: ");
-            scanf("%f", &x);
-            printf("Masukkan nilai kedua: ");
-            scanf("%f", &y);
-            printf("Hasil penjumlahan antara %.2f dan %.2f adalah %.2f", x, y, x + y);
-        }else if(pil == 2){
-            printf("Masukkan nilai pertama: ");
-            scanf("%f", &x);
-            printf("Masukkan nilai kedua: ");
-            scanf("%f", &y);
-            printf("Hasil pengurangan antara %.2f dan %.2f adalah %.2f", x, y, x - y);  
-        }else if(pil == 3){
-            printf("Masukkan nilai pertama: ");
-            scanf("%f", &x);
-            printf("Masukkan nilai kedua: ");
-            scanf("%f", &y);
-            printf("Hasil perkalian antara %.2f dan %.2f adalah %.2f", x, y, x * y);
-        }else if(pil == 4){
-            printf("Masukkan nilai pertama: ");
-            scanf("%f", &x);
-            printf("Masukkan nilai kedua: ");
-            scanf("%f", &y);
-            printf("Hasil pembagian antara %.2f dan %.2f adalah %.2f", x, y, x / y);
-        }else{
-            printf("Input anda salah, silahkan coba lagi\n");
+        scanf("%d", &pil);
+        switch(pil){
+            case 1:
+                kalk(&x, &y);
+                printf("Hasil penjumlahan antara %.2f dan %.2f adalah %.2f\n", x, y, x + y);
+                break;
+            case 2:
+                kalk(&x, &y);
+                printf("Hasil pengurangan antara %.2f dan %.2f adalah %.2f\n", x, y, x - y);
+                break;
+            case 3:
+                kalk(&x, &y);
+                printf("Hasil perkalian antara %.2f dan %.2f adalah %.2f\n", x, y, x * y);
+                break;
+            case 4:
+                kalk(&x, &y);
+                printf("Hasil pembagian antara %.2f dan %.2f adalah %.2f\n", x, y, x / y);
+                break;
+            case 5:
+                printf("Terimakasih, telah menggunakan kalkulator Allano");
+                return 0;
+            default:
+                printf("Input anda salah, silahkan coba lagi\n");
+                break;
         }
-                
-            }
-            return 0;
         }
+    }
